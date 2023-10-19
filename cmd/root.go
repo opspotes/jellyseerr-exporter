4ff5a995fd1445eeedba6ce261a01fd0bc71ca39
Kyâne <kyane@kyane.fr>
2023-10-19 23:01:36 +0200

    Allow to disable genre/company collection

--- cmd/root.go
+++ cmd/root.go
@@ -19,9 +19,7 @@ var (
 	listenAddress      string
 	metricsPath        string
 	overseerrAPILocale string
-
-	scrapeGenres    bool
-	scrapeCompanies bool
+	fullData     bool
 )
 
 // instance to use
@@ -43,8 +41,8 @@ var RootCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		prometheus.MustRegister(prometheus.NewBuildInfoCollector())
-		prometheus.MustRegister(collector.NewRequestCollector(overseerr, scrapeGenres, scrapeCompanies))
 		prometheus.MustRegister(collector.NewUserCollector(overseerr))
+		prometheus.MustRegister(collector.NewRequestCollector(jellyseerr, fullData))
 
 		handler := promhttp.Handler()
 		http.Handle(metricsPath, handler)
@@ -87,10 +85,9 @@ func init() {
 	RootCmd.PersistentFlags().StringVar(&overseerrAddress, "overseerr.address", "", "Address at which Overseerr is hosted.")
 	RootCmd.PersistentFlags().StringVar(&overseerrAPIKey, "overseerr.api-key", "", "API key for admin access to the Overseerr instance.")
 	RootCmd.PersistentFlags().StringVar(&overseerrAPILocale, "overseerr.locale", "en", "Locale of the Overseerr instance.")
-	RootCmd.PersistentFlags().BoolVar(&scrapeGenres, "overseerr.scrape.genres", true, "Scrape genere details from the media requests.")
-	RootCmd.PersistentFlags().BoolVar(&scrapeGenres, "overseerr.scrape.companies", true, "Scrape company/network details from the media requests.")
 	RootCmd.MarkPersistentFlagRequired("overseerr.address")
 	RootCmd.MarkPersistentFlagRequired("overseerr.api-key")
+	RootCmd.PersistentFlags().BoolVar(&fullData, "fullData", false, "Reduce scraping and cardinality on requests count metric.")
 
 	// setup vars (based on ha proxy exporter)
 	RootCmd.PersistentFlags().StringVar(&listenAddress, "web.listen-address", ":9850", "Address to listen on for web interface and telemetry.")
